6ce9a3780c2a14377d407354bf29c37f6af9869d
KonstantinSeA <Acidvein@yandex.ru>
2024-02-05 08:27:02 +0300

    наброски flask/Реклама с картинкой

--- Kadet.py
+++ Kadet.py
@@ -50,8 +50,34 @@ def promotion_image():
                   </head>
                   <body>
                     <h1>Жди нас, Марс!</h1>
+                    <img src="{url_for('static', filename='img/Mars.png')}" 
+                    alt="здесь должна была быть картинка, но не нашлась">
                   </body>
                 </html>"""
+
+
+@app.route('/bootstrap_sample')
+def bootstrap():
+    return '''<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                    <link rel="stylesheet" 
+                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                    crossorigin="anonymous">
+                    <link rel="stylesheet" type="text/css" 
+                    href="{url_for('static', filename='css/style.css')}"/>
+                    <title>Привет, Яндекс!</title>
+                  </head>
+                  <body>
+                    <h1>Привет, Яндекс!</h1>
+                    <div class="alert alert-primary" role="alert">
+                      А мы тут компонентами Bootstrap балуемся
+                    </div>
+                  </body>
+                </html>'''
 
 
 if __name__ == '__main__':

3f4c705eb0fc94479d6cf24c10ab6be24770eec6
KonstantinSeA <Acidvein@yandex.ru>
2024-02-08 13:43:14 +0300

    релиз flask/Варианты выбора

--- Kadet.py
+++ Kadet.py
@@ -182,5 +182,43 @@ def astronaut_selection():
         return "Форма отправлена"
 
 
+@app.route('/choice/<string:planet_name>')
+def choice(planet_name):
+    code = f"""
+        <!doctype html>
+        <html lang="en">
+          <head>
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+            <link rel="stylesheet" 
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+            integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+            crossorigin="anonymous">
+            <link rel="stylesheet" type="text/css" 
+            href="{url_for('static', filename='css/style.css')}" />
+            <title>Колонизация</title>
+          </head>
+          <body>
+            <h1 align="center">Мое предложение: {planet_name.capitalize()}!</h1>
+            <div class="alert alert-secondary" role="alert">
+              Эта планета близка к Земле;
+            </div>
+            <div class="alert alert-success" role="alert">
+              На ней много необходимых ресурсов;
+            </div>
+            <div class="alert alert-dark" role="alert">
+              На ней есть вода и атмосфера;
+            </div>
+            <div class="alert alert-warning" role="alert">
+              На ней есть небольшое магнитное поле;
+            </div>
+            <div class="alert alert-danger" role="alert">
+              Наконец, она просто красива!
+            </div>
+          </body>
+        </html>"""
+    return code
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')

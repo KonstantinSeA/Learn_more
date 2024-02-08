bc6c450a7ee9007378c167a809bfff5ab3343f38
KonstantinSeA <Acidvein@yandex.ru>
2024-02-08 13:54:16 +0300

    релиз flask/Результат отбора

--- Kadet.py
+++ Kadet.py
@@ -220,5 +220,36 @@ def choice(planet_name):
     return code
 
 
+@app.route('/results/<nickname>/<int:level>/<float:rating>')
+def results(nickname, level, rating):
+    code = f"""
+            <!doctype html>
+            <html lang="en">
+              <head>
+                <meta charset="utf-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                <link rel="stylesheet" 
+                href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                crossorigin="anonymous">
+                <link rel="stylesheet" type="text/css" 
+                href="{url_for('static', filename='css/style.css')}" />
+                <title>Колонизация</title>
+              </head>
+              <body>
+                <h1>Результаты отбора</h1>
+                <h4>Претендента на участие в миссии {nickname.capitalize()}</h4>
+                <div class="alert alert-success" role="alert">
+                  Поздравляем! Ваш рейтинг после {str(level)} этапа отбора
+                </div>
+                <h4>составляет {str(rating)}!</h4>
+                <div class="alert alert-warning" role="alert">
+                  Желаем удачи!
+                </div>
+              </body>
+            </html>"""
+    return code
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')

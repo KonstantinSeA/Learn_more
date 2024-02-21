61b83460f77f92d0b0ac5e4dbe90d91451e87113
KonstantinSeA <Acidvein@yandex.ru>
2024-02-21 19:53:55 +0300

    Релиз Репозитории/Эспрессо

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+import sqlite3
 
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QSpinBox, QTextBrowser
@@ -19,6 +20,8 @@ class Main(QWidget):
         self.Descript_name_label.setStyleSheet('color: #fffcff;')
         self.Price_title_label.setStyleSheet('color: #fffcff;')
         self.Price_view_label.setStyleSheet('color: #fffcff;')
+        self.Type_title_label.setStyleSheet('color: #fffcff;')
+        self.Type_view_label.setStyleSheet('color: #fffcff;')
         self.Volume_title_label.setStyleSheet('color: #fffcff;')
         self.Volume_view_label.setStyleSheet('color: #fffcff;')
 
@@ -30,7 +33,20 @@ class Main(QWidget):
         self.Descript_textBrowser.setStyleSheet('background: #466363; color: #fffcff;')
 
     def run_search(self):
-        pass
+        con = sqlite3.connect(f'coffee.sqlite')
+        cur = con.cursor()
+        result = cur.execute(
+            f"""SELECT id, sort, degree_of_roasting, type, description, price, volume FROM coffee 
+                    WHERE id = '{self.Id_spinBox.text()}'""").fetchall()
+        if len(result) > 0:
+            self.Id_view_label.setText(str(result[0][0]))
+            self.Name_view_label.setText(str(result[0][1]))
+            self.Degree_view_label.setText(str(result[0][2]))
+            self.Type_view_label.setText(str(result[0][3]))
+            self.Price_view_label.setText(str(result[0][5]))
+            self.Volume_view_label.setText(str(result[0][6]))
+
+            self.Descript_textBrowser.setText(str(result[0][4]))
 
 
 if __name__ == '__main__':

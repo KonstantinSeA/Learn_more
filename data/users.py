861139b8bf58b33e887ba43abf6dcb9d134dcba0
KKA <Acidvein@yandex.ru>
2024-02-24 10:46:08 +0300

    Наработки с первого занятия

--- data/users.py
+++ data/users.py
@@ -1,21 +1,24 @@
-import datetime
 import sqlalchemy
+import datetime
+from sqlalchemy import orm
 from .db_session import SqlAlchemyBase
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class User(SqlAlchemyBase):
     __tablename__ = 'users'
 
-    id = sqlalchemy.Column(sqlalchemy.Integer,
-                           primary_key=True, autoincrement=True)
-    surname = sqlalchemy.Column(sqlalchemy.String)
-    name = sqlalchemy.Column(sqlalchemy.String)
-    age = sqlalchemy.Column(sqlalchemy.Integer)
-    position = sqlalchemy.Column(sqlalchemy.String)
-    speciality = sqlalchemy.Column(sqlalchemy.String)
-    address = sqlalchemy.Column(sqlalchemy.String)
-    email = sqlalchemy.Column(sqlalchemy.String, unique=True)
-    hashed_password = sqlalchemy.Column(sqlalchemy.String)
-    modified_date = sqlalchemy.Column(sqlalchemy.DateTime)
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    about = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    email = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    created_date = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.now)
 
-    job = orm.relationship("Job", back_populates='user')
+    news = orm.relationship("News", back_populates='user')
+
+    def set_password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.hashed_password, password)
--- main.py
+++ main.py
@@ -1,24 +1,31 @@
-from flask import Flask
-
+from flask import Flask, render_template, redirect
 from data import db_session
-from werkzeug.security import generate_password_hash, check_password_hash
+from data.users import User
+from data.news import News
+from forms.user import RegisterForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
-'''def reqister():
+@app.route('/')
+def index():
+    db_sess = db_session.create_session()
+    news = db_sess.query(News).all()
+    return render_template("index.html", news=news)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
     form = RegisterForm()
     if form.validate_on_submit():
         if form.password.data != form.password_again.data:
             return render_template('register.html', title='Регистрация',
-                                   form=form,
-                                   message="Пароли не совпадают")
+                                   form=form, message='Пароли разные')
         db_sess = db_session.create_session()
         if db_sess.query(User).filter(User.email == form.email.data).first():
             return render_template('register.html', title='Регистрация',
-                                   form=form,
-                                   message="Такой пользователь уже есть")
+                                   form=form, message='Пользователь уже существует')
         user = User(
             name=form.name.data,
             email=form.email.data,
@@ -27,14 +34,14 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
         user.set_password(form.password.data)
         db_sess.add(user)
         db_sess.commit()
-        return redirect('/login')
-    return render_template('register.html', title='Регистрация', form=form)'''
+        return redirect('/')
+    return render_template('register.html', title='Регистрация',
+                           form=form)
 
 
 def main():
-    db_session.global_init("db/blogs.db")
+    db_session.global_init('db/blogs.db')
     app.run()
-
 
 if __name__ == '__main__':
     main()

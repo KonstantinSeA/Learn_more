cf2e1712659dab709acbc148cac60e8e41a8f340
KKA <Acidvein@yandex.ru>
2024-02-24 12:20:14 +0300

    Новые наработки по штукам

--- main.py
+++ main.py
@@ -1,17 +1,35 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, request, make_response
+from flask import session
 from data import db_session
 from data.users import User
 from data.news import News
 from forms.user import RegisterForm
+from forms.login import LoginForm
+import datetime as dt
+from flask_login import LoginManager, login_user, login_required, logout_user
+from flask_login import current_user
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+app.config['PERMANENT_SESSION_LIFETIME'] = dt.timedelta(days=365)
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
 
 
 @app.route('/')
 def index():
     db_sess = db_session.create_session()
-    news = db_sess.query(News).all()
+    if current_user.is_authenticated:
+        news = db_sess.query(News).filter(
+            (News.user == current_user) | (News.is_private == False)).all()
+    else:
+        news = db_sess.query(News).filter(News.is_private == False).all()
     return render_template("index.html", news=news)
 
 
@@ -37,6 +55,47 @@ def register():
         return redirect('/')
     return render_template('register.html', title='Регистрация',
                            form=form)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect('/')
+
+
+@app.route('/cookie')
+def cookie_test():
+    visits_count = int(request.cookies.get('visits_count', 0))
+    if visits_count:
+        ans = make_response(f"Вы пришли на страницу {visits_count + 1} раз")
+        ans.set_cookie('visits_count', str(visits_count + 1),
+                       max_age=60 * 60 * 24)
+    else:
+        ans = make_response(f"Вы пришли на страницу в первый раз за сутки")
+        ans.set_cookie('visits_count', '1', max_age=60 * 60 * 24)
+    return ans
+
+
+@app.route('/session')
+def session_test():
+    visits_count = session.get('visits_count', 0)
+    session['visits_count'] = visits_count + 1
+    return make_response(f"Вы пришли на страницу {visits_count + 1} раз")
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect('/')
+        return render_template('login.html', title='Авторизация',
+                               message='Неверный логин или пароль', form=form)
+    return render_template('login.html', title='Авторизация', form=form)
 
 
 def main():

03cbbffa35ae3d85c3672c99f8c53c7ade8db792
KonstantinSeA <Acidvein@yandex.ru>
2024-03-24 13:46:01 +0300

    Релиз flask/Загрузка файла

--- Kadet.py
+++ Kadet.py
@@ -26,14 +26,15 @@ def astronaut_selection():
             href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
             integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
             crossorigin="anonymous">
-            <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+            <link rel="stylesheet" 
+              type="text/css" href="{url_for('static', filename='css/style.css')}" />
             <title>Отбор астронавтов</title>
             </head>
           <body>
             <h1 align="center">Анкета Претендента</h1>
             <h2 align="center">на участие в миссии</h2>
             <div>
-              <form class="selection_form" method="post">
+              <form class="selection_form" method="post" enctype="multipart/form-data">
                 <div class="form-group">
                   <label for="photo">Приложите фотографию</label>
                   <input type="file" class="form-control-file" id="photo" name="file">
@@ -44,37 +45,40 @@ def astronaut_selection():
           </body>
         </html>'''
     elif request.method == 'POST':
-        print(request.form)
+        f = request.files['file']
+        with open(f'static/img/{f.filename}', mode='wb') as fod:
+            fod.write(f.read())
         return f'''
-                <!doctype html>
-                <html lang="en">
-                  <head>
-                    <meta charset="utf-8">
-                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-                    <link rel="stylesheet"
-                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
-                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
-                    crossorigin="anonymous">
-                    <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
-                    <title>Отбор астронавтов</title>
-                    </head>
-                  <body>
-                    <h1 align="center">Анкета Претендента</h1>
-                    <h2 align="center">на участие в миссии</h2>
-                    <div>
-                      <form class="selection_form" method="post">
-                        <div class="form-group">
-                          <label for="photo">Приложите фотографию</label>
-                          <input type="file" class="form-control-file" id="photo" name="file">
-                        </div>
-                        <img src="{dir(request.form['file'])}" 
-                         width="427" height="427"
-                         alt="здесь должна была быть картинка, но не нашлась">
-                        <button type="submit" class="btn btn-primary">Отправить</button>
-                      </form>
-                    </div>
-                  </body>
-                </html>'''
+    <!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+            integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+              crossorigin="anonymous">
+        <link 
+          rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+        <title>Отбор астронавтов</title>
+    </head>
+    <body>
+        <h1 align="center">Анкета Претендента</h1>
+        <h2 align="center">на участие в миссии</h2>
+        <div>
+            <form class="selection_form" method="post" enctype="multipart/form-data">
+                <div class="form-group">
+                    <label for="photo">Приложите фотографию</label>
+                    <input type="file" class="form-control-file" id="photo" name="file">
+                </div>
+                <img src="{url_for('static', filename='img/' + f.filename)}" 
+                  width="427" height="427"
+                  alt="здесь должна была быть картинка, но не нашлась">
+                <button type="submit" class="btn btn-primary">Отправить</button>
+            </form>
+        </div>
+    </body>
+</html>'''
 
 
 if __name__ == '__main__':
